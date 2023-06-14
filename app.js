38f402b182aa6bd86d19ef6b1ce42d4a77cb848c
Sathwika538 <97402887+Sathwika538@users.noreply.github.com>
2023-06-14 14:13:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -49,7 +49,11 @@ if(process.env.NODE_ENV !== "PRODUCTION"){
 app.use('/api/v1',user);
 //database connection
 const connect = require("./database");
-
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key:process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.CLOUDINARY_API_SECRET,
+})
 
 const mongoose = require('mongoose');
 
